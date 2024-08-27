9506fe4f38d9538a18b763d877516094d6e3fcf1
mahadihassan-dev <mahadihassan.mhd@gmail.com>
2024-08-27 20:56:34 +0600

    Final

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        300:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+  
